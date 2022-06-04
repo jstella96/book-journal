ac97ccc0829024ad4608feddb7e9bfbc7e8f071c
jstella96 <jstella0530@naver.com>
2022-06-04 12:23:14 +0900

    [#15] feat: Sort Component 구현

--- client/src/components/sidefilter/Sort.js
+++ client/src/components/sidefilter/Sort.js
@@ -1,14 +1,14 @@
 
-export default function Sort({$target, initialState={}, onEvent}){
-  //[필수]
+export default function Sort({$target, initialState={}, onClick}){
+
   this.$element = document.createElement('div'); 
   this.$element.className = "side-filter__sort"
   
-  /* 이름 종속되게 짓지 말기 */
   this.state = {
-
+    sort: initialState.sort ? initialState.sort : [],
+    selectedIndex: 0
   }
-  //this.state = initialState
+
   $target.appendChild(this.$element)
 
   this.setState = (nextState) => {
@@ -20,20 +20,28 @@ export default function Sort({$target, initialState={}, onEvent}){
   }
   
   this.render = () => {
+    const { sort,  selectedIndex} = this.state
+    const sortTemplate = sort.map( (sortBy,index) => 
+        `
+        <li class="side-filter__button ${index ==  selectedIndex ? 'side-filter__button--selected' : ''}">
+          <span class="side-filter__name">${sortBy.name}</span>
+        </li>
+        `
+    ).join('')
     this.$element.innerHTML = `
-   
-    <h3 class="side-filter__title">정렬기준</h3>
-    <ul>
-      <li class="side-filter__button side-filter__button--selected">
-          <span class="side-filter__name">작성날짜</span>
-      </li>
-      <li class="side-filter__button">
-          <span class="side-filter__name">좋아요</span>
-     </li>
-    </ul>
- 
+      <h3 class="side-filter__title">정렬기준</h3>
+      <ul>${sortTemplate}</ul>
     `
   }
-  
+
+  this.$element.addEventListener('click',(e)=>{
+    const $li = e.target.closest('li')
+      if ($li) {
+        const {sort, selectedIndex} = this.state
+        onClick(sort[selectedIndex]);
+      }
+  })
+
+
   this.render()
 }
