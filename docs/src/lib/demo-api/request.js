443e8cc98fd829fdcf03a7adcb3870d04365e88a
jstella96 <jstella0530@naver.com>
2022-06-08 21:53:13 +0900

    fix: API END POINT 에 repo 경로 추가

--- docs/src/lib/demo-api/request.js
+++ docs/src/lib/demo-api/request.js
@@ -3,7 +3,7 @@ point!
 1. fetch는 반복적으로 사용되기 때문에 기본 구조를 만들고 필요시 호출하여 사용한다.
 2. 필요한 페이지 혹은 컴포넌트에서 request를 호출 하는 방법도 있으나(밑에 주석). 요청api따로 관리하는 것도 좋다.
 */
-const API_END_POINT = '/src/assets/json'//환경변수 사용하기- 추후 수정 사항 
+const API_END_POINT = '/book-journal/src/assets/json'//환경변수 사용하기- 추후 수정 사항 
 
 const statusErrorFromResponse = (res) => {
   if (res.status < 300) return '';
